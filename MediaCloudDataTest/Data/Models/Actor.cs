4c1de0dd9f5889d5cdf84fac020c711ed22bd98b
KillReal <r.timo086@gmail.com>
2022-11-01 23:01:03 +0300

    Project cleanup

--- MediaCloudDataTest/Data/Models/Actor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MediaCloud.Data.Models
-{
-    public class Actor
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string PasswordHash { get; set; }
-    }
-}
