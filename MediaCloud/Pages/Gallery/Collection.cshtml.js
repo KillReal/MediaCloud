14f635dd13091985f5c54f6440c009b812fd1bb4
KillReal <r.timo086@gmail.com>
2024-11-14 18:08:47 +0300

    Remove returnUrl parameter

--- MediaCloud/Pages/Gallery/Collection.cshtml.js
+++ MediaCloud/Pages/Gallery/Collection.cshtml.js
@@ -22,7 +22,7 @@ function CreateActualCard(data, id, collectionId) {
     cardDiv.id = id;
     cardDiv.onclick = function (e) {
         var id = e.currentTarget.id;
-        window.location = "Detail?id=" + data[id].id + "&returnUrl=/Gallery/Collection?id=" + collectionId + "&rootReturnUrl=" + rootReturnUrl;
+        window.location = "Detail?id=" + data[id].id;
     }
 
     // With image
