52eb6582cdf6f714ba640b5e25bf9503bb626f04
KillReal <r.timo086@gmail.com>
2024-11-23 21:35:19 +0300

    Make error page authorized

--- MediaCloud/Pages/Error.cshtml.cs
+++ MediaCloud/Pages/Error.cshtml.cs
@@ -1,23 +1,25 @@
+using MediaCloud.WebApp.Pages;
+using MediaCloud.WebApp.Services.UserProvider;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SixLabors.ImageSharp.PixelFormats;
 using System.Diagnostics;
 
 namespace MediaCloud.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
-    public class ErrorModel(ILogger<ErrorModel> logger) : PageModel
+    public class ErrorModel(IUserProvider userProvider) : AuthorizedPageModel(userProvider)
     {
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-        private readonly ILogger<ErrorModel> _logger = logger;
+        public string ErrorMessage { get; set; }
 
         public void OnGet(string message = "Unknown error")
         {
-            _logger.LogInformation("Error occured during request: {HttpContext.Request.Path} by {HttpContext.User.Identity.Name} message: {message}", 
-                HttpContext.Request.Path, HttpContext.User.Identity?.Name, message);
+            ErrorMessage = message;
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
         }
     }
