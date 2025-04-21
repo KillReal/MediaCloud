166503d6e420ad89e6520abdf8e8ce7b2eeb65e3
KillReal <r.timo086@gmail.com>
2025-04-21 13:13:59 +0300

    feat: Changed tags color ordering

--- MediaCloud/Data/Types/TagColor.cs
+++ MediaCloud/Data/Types/TagColor.cs
@@ -4,21 +4,23 @@ namespace MediaCloud.Data.Types
 {
     public enum TagColor
     {
-        [Display(Name = "Blue")]
-        Blue = 0,
-        [Display(Name = "Purple")]
-        Purple = 1,
         [Display(Name = "Orange")]
-        Orange = 2,
+        Orange = 1,
+        [Display(Name = "Purple")]
+        Purple = 2,
         [Display(Name = "Red")]
         Red = 3,
+        [Display(Name = "Pink")]
+        Pink = 4,
+        [Display(Name = "Blue")]
+        Blue = 5,
         [Display(Name = "Green")]
-        Green = 4,
-        [Display(Name = "Aquamarine")]
-        Aquamarine = 5,
-        [Display(Name = "Black")]
-        Black = 6,
+        Green = 6,
         [Display(Name = "White")]
-        White = 7
+        White = 7,
+        [Display(Name = "Black")]
+        Black = 8,
+        [Display(Name = "Aquamarine")]
+        Aquamarine = 9,
     }
 }
